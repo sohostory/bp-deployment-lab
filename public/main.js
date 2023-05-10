5e749dd06219e1718cf0e8a751bce6e6d7baedfd
sohostory <sohostory@gmail.com>
2023-05-10 14:46:28 -0500

    added another button

--- public/main.js
+++ public/main.js
@@ -5,4 +5,4 @@ const button1Handler = () => alert("stop clicking on me!");
 const button2Handler = () => alert("i said! stop clicking on me!");
 
 button1.addEventListener("click", button1Handler);
-button2.addEventListener("click", button1Handler);
+button2.addEventListener("click", button2Handler);
