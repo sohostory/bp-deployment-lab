66bcb7acf9fbbff566dbbb7b42ffe07d0adfb4f3
sohostory <sohostory@gmail.com>
2023-05-11 14:02:23 -0500

    added rollbar

--- public/main.js
+++ public/main.js
@@ -1,8 +1,25 @@
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
+const wordDiv = document.querySelector("#word");
+const wordP = document.querySelector("#wordP");
+const wordBtn = document.querySelector("#wordBtn");
 
-const button1Handler = () => alert("stop clicking on me!");
-const button2Handler = () => alert("i said! stop clicking on me!");
+const baseURL = "http://18.220.250.73";
+
+const button1Handler = () => {
+  alert("stop clicking on me!");
+};
+const button2Handler = () => {
+  alert("i said! stop clicking on me!");
+};
+
+const getWord = () => {
+  axios.get(`${baseURL}/word`).then((res) => {
+    const data = res.data;
+    wordP.textContent = data;
+  });
+};
 
 button1.addEventListener("click", button1Handler);
 button2.addEventListener("click", button2Handler);
+wordBtn.addEventListener("click", getWord);
--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 app.use(express.static(`${__dirname}/public`));
 
+const { ROLLBAR_ACCESS_TOKEN } = process.env;
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: ROLLBAR_ACCESS_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
+app.get("/word", (req, res) => {
+  rollbar.info("the user got a word");
+  res.status(200).send("word");
+});
 app.listen(4000, () => console.log(`server running on 4000`));
